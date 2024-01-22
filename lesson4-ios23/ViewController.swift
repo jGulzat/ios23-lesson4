7df8d140a1fb10b548025801f6edab003580edec
jGulzat <gulzatj97@mail.ru>
2024-01-22 19:13:23 +0600

    Initial Commit

---
+++ lesson4-ios23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson4-ios23
+//
+//  Created by Gulzat Zheenbek kyzy on 22/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
